3958a166818265dd6ff30fef8cdcab98c1c3f05f
Jhoselin Tumiri Quiroz <jtumiriqdaw1@gmail.com>
2019-10-30 22:52:19 +0100

    TestPedido en proceso ..(probando)

--- Pedido.java
+++ Pedido.java
@@ -4,6 +4,7 @@
  * Modela un pedido realizado por un cliente en una determinada fecha
  * El pedido incluye dos líneas de pedido que describen a cada uno de los dos
  * productos comprados en el pedido
+ * @autor --Jhoselin Tumiri Quiroz---
  */
 public class Pedido
 {
@@ -38,45 +39,45 @@ public class Pedido
  }
       
  /**
-     * calcular y devolver el importe total del pedido sin Iva
-     */
-    public  double getImporteAntesIva() {
+ * calcular y devolver el importe total del pedido sin Iva
+ */
+ public  double getImporteAntesIva() {
     double importe1 = linea1.getCantidad() * linea1.getProducto().getPrecio();
     double importe2 = linea2.getCantidad() * linea2.getProducto().getPrecio();
     double importeAntesIva = importe1 + importe2;
     return importeAntesIva;
-    }
+ }
 
  /**
-     * calcular y devolver el iva a aplicar
-     */
-    public double  getIva() {
+ * calcular y devolver el iva a aplicar
+ */
+ public double  getIva() {
     double importe1 = linea1.getCantidad() * linea1.getProducto().getPrecio() * IVA;
     double importe2 = linea2.getCantidad() * linea2.getProducto().getPrecio() * IVA;
     double importeIva = importe1 + importe2;
     return importeIva;
  }
 
-    /**
-     * calcular y devolver el importe total del pedido con Iva
-     */
-    public double getImporteTotal() {
-         double importeTotal = getImporteAntesIva() + getIva();
-         return importeTotal;
-    }
+ /**
+ * calcular y devolver el importe total del pedido con Iva
+ */
+ public double getImporteTotal() {
+    double importeTotal = getImporteAntesIva() + getIva();
+    return importeTotal;
+ }
 
-    /**
-     * Representación textual del pedido
-     * (ver enunciado)
-     */
-    public String toString() {
-       String str = String.format("\n%20s%8.2f \n%20s%8.2f \n%20s%8.2f",
-       "IMPORTE SIN IVA:", mporteAntesIva, "IVA:", getIva, "IMPORTE TOTAL:",
-        getImporteTotal);
-        return str;
+ /**
+ * Representación textual del pedido
+ * (ver enunciado)
+ */
+ public String toString() {
+    String str = String.format("\n%20s%8.2f \n%20s%8.2f \n%20s%8.2f",
+    "IMPORTE SIN IVA:", getImporteAntesIva(), "IVA:", getIva(), "IMPORTE TOTAL:",
+    getImporteTotal());
+    return str;
       
       
-    }
+ }
     
     
     // /**
@@ -84,7 +85,8 @@ public class Pedido
      // * como parámetro
      // */
     // public boolean masAntiguoQue(Pedido otro) {
-         
+         // if(Pedido)
+            // Fecha
     // }
     
      // /**
--- TestPedido.java
+++ TestPedido.java
@@ -1,24 +1,32 @@
 
 /**
  *  Clase para probar el resto de clases
- *  
+ *  @autor---Jhoselin Tumiri Quiroz---
  */
 public class TestPedido
 {
     private Pedido pedido1; 
     private Pedido pedido2; 
-
+    
     /**
      * Constructor  
      */
-    public TestPedido()    {
+    public TestPedido(Cliente cliente1 )    {
         // crear pedido1
+        pedido1 = new Pedido(new Fecha(4,9,2019), new Cliente("Juan Soto","Avda. Pio XII", "Pamplona", "Navarra"), 
+                   new LineaPedido(new Producto("Rotulador fosforescente", 6.70), 20),
+                  new LineaPedido(new Producto("Memoria USB 64GB", 14.80), 10));
+                  
+        
+        
+    
+        
        
-	   
-	   
-	   
 
         // crear pedido2
+        pedido2 = new Pedido(new Fecha(8,10,2019), new Cliente("Elisa Nuin","Río Alzania 7", "Pamplona", "Navarra"), 
+                   new LineaPedido(new Producto("Sacapuntas manual", 16.64), 8),
+                  new LineaPedido(new Producto("Corrector tippex", 5.99), 20));
          
 
     }
